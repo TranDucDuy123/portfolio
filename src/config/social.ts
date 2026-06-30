8993131126dc731593f121ce3570aff0950162a0
duy1233 <dyly399@gmail.com>
2026-06-30 08:11:00 +0700

    okla

--- src/config/social.ts
+++ src/config/social.ts
@@ -11,13 +11,13 @@ export interface SocialConfig {
 }
 
 export const socialConfig: SocialConfig = {
-  zalo: "https://zalo.me/0909123456", // placeholder for user to update
-  phone: "0909123456",
-  phoneDisplay: "0909.123.456",
+  zalo: "https://zalo.me/0938415218", // placeholder for user to update
+  phone: "0938415218",
+  phoneDisplay: "0938.415.218",
   facebookPersonal: "https://facebook.com/ducduy.web",
   facebookFanpage: "https://facebook.com/ducduyweb.hub",
   youtube: "https://youtube.com/@ducduyweb",
   email: "contact@ducduyweb.vn",
-  qrZalo: "https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=https://zalo.me/0909123456",
+  qrZalo: "https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=https://zalo.me/0938415218",
   address: "Quận 1, Thành phố Hồ Chí Minh, Việt Nam"
 };
